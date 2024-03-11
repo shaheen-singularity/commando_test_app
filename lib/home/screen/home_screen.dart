970dbbef0d80ac7986a96b332bf6297cae34e182
Aminul Islam <shaheen.singularity@gmail.com>
2024-03-11 13:20:01 +0600

    added: card design added

--- lib/home/screen/home_screen.dart
+++ lib/home/screen/home_screen.dart
@@ -1,10 +1,232 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  String status = "inactive";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return  Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Gap(64),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder( //<-- SEE HERE
+                  borderSide: BorderSide(
+                      width: 1, color: Colors.redAccent),
+                ),),
+            ),
+            Card(
+              child: ListTile(
+                // tileColor: colorScheme.primaryContainer,
+                onTap: () {
+
+                },
+                title: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Smoke",
+                        style: textTheme.headlineMedium?.copyWith(color: Colors.black),
+                      ),
+                      if (status != "active")
+                        WidgetSpan(
+                          child: Badge(
+                            label: Text(status?.capitalizeFirst ?? ""),
+                            backgroundColor: Colors.red.withOpacity(0.1),
+                            textColor: Colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                trailing: ToggleSwitch(
+                  minWidth: 65,
+                  minHeight: 25.0,
+                  initialLabelIndex: status == "active" ? 1 : 0,
+                  cornerRadius: 8.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  labels: const ["Inactive", "Active"],
+                  fontSize: 8,
+                  customTextStyles: const [TextStyle(fontWeight: FontWeight.bold)],
+                  activeBgColors: const [
+                    [Colors.red],
+                    [Colors.green]
+                  ],
+                  animate: true,
+                  curve: Curves.easeOut,
+                  onToggle: (index) {
+                    String selectedOption = ['inactive', 'active'][index!];
+                    print(selectedOption);
+
+                  },
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                // tileColor: colorScheme.primaryContainer,
+                onTap: () {
+
+                },
+                title: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Smoke",
+                        style: textTheme.headlineMedium?.copyWith(color: Colors.black),
+                      ),
+                      if (status != "active")
+                        WidgetSpan(
+                          child: Badge(
+                            label: Text(status?.capitalizeFirst ?? ""),
+                            backgroundColor: Colors.red.withOpacity(0.1),
+                            textColor: Colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                trailing: ToggleSwitch(
+                  minWidth: 65,
+                  minHeight: 25.0,
+                  initialLabelIndex: status == "active" ? 1 : 0,
+                  cornerRadius: 8.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  labels: const ["Inactive", "Active"],
+                  fontSize: 8,
+                  customTextStyles: const [TextStyle(fontWeight: FontWeight.bold)],
+                  activeBgColors: const [
+                    [Colors.red],
+                    [Colors.green]
+                  ],
+                  animate: true,
+                  curve: Curves.easeOut,
+                  onToggle: (index) {
+                    String selectedOption = ['inactive', 'active'][index!];
+                    print(selectedOption);
+
+                  },
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                // tileColor: colorScheme.primaryContainer,
+                onTap: () {
+
+                },
+                title: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Smoke",
+                        style: textTheme.headlineMedium?.copyWith(color: Colors.black),
+                      ),
+                      if (status != "active")
+                        WidgetSpan(
+                          child: Badge(
+                            label: Text(status?.capitalizeFirst ?? ""),
+                            backgroundColor: Colors.red.withOpacity(0.1),
+                            textColor: Colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                trailing: ToggleSwitch(
+                  minWidth: 65,
+                  minHeight: 25.0,
+                  initialLabelIndex: status == "active" ? 1 : 0,
+                  cornerRadius: 8.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  labels: const ["Inactive", "Active"],
+                  fontSize: 8,
+                  customTextStyles: const [TextStyle(fontWeight: FontWeight.bold)],
+                  activeBgColors: const [
+                    [Colors.red],
+                    [Colors.green]
+                  ],
+                  animate: true,
+                  curve: Curves.easeOut,
+                  onToggle: (index) {
+                    String selectedOption = ['inactive', 'active'][index!];
+                    print(selectedOption);
+
+                  },
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                // tileColor: colorScheme.primaryContainer,
+                onTap: () {
+
+                },
+                title: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Smoke",
+                        style: textTheme.headlineMedium?.copyWith(color: Colors.black),
+                      ),
+                      if (status != "active")
+                        WidgetSpan(
+                          child: Badge(
+                            label: Text(status?.capitalizeFirst ?? ""),
+                            backgroundColor: Colors.red.withOpacity(0.1),
+                            textColor: Colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                trailing: ToggleSwitch(
+                  minWidth: 65,
+                  minHeight: 25.0,
+                  initialLabelIndex: status == "active" ? 1 : 0,
+                  cornerRadius: 8.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  labels: const ["Inactive", "Active"],
+                  fontSize: 8,
+                  customTextStyles: const [TextStyle(fontWeight: FontWeight.bold)],
+                  activeBgColors: const [
+                    [Colors.red],
+                    [Colors.green]
+                  ],
+                  animate: true,
+                  curve: Curves.easeOut,
+                  onToggle: (index) {
+                    String selectedOption = ['inactive', 'active'][index!];
+                    print(selectedOption);
+
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:commando_test_app/home/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
